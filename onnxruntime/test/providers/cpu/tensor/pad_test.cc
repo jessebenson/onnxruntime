3e66321c6e31eb8583d8ec911bb4b95763632969
Jesse Benson <jesse.benson@microsoft.com>
2019-01-20 22:34:16 -0800

    Add a quick, reproducible Pad performance unit test.

--- onnxruntime/test/providers/cpu/tensor/pad_test.cc
+++ onnxruntime/test/providers/cpu/tensor/pad_test.cc
@@ -3,9 +3,87 @@
 
 #include "gtest/gtest.h"
 #include "test/providers/provider_test_utils.h"
+#include "test/util/include/default_providers.h"
+#include "test/framework/test_utils.h"
+#include "core/session/inference_session.h"
+
+#include <chrono>
+#include <sstream>
 
 namespace onnxruntime {
 namespace test {
+
+TEST(TensorOpTest, Pad_Performance) {
+  // Create graph with single Pad node
+  IOnnxRuntimeOpSchemaRegistryList custom_schema_registries;
+  std::unordered_map<std::string, int> domain_to_version;
+  domain_to_version[onnxruntime::kOnnxDomain] = 7;
+  auto model = std::make_unique<onnxruntime::Model>("test", false, ModelMetaData(), custom_schema_registries, domain_to_version);
+  onnxruntime::Graph& graph = model->MainGraph();
+
+  ONNX_NAMESPACE::TypeProto in_type;
+  in_type.mutable_tensor_type()->set_elem_type(ONNX_NAMESPACE::TensorProto_DataType_FLOAT);
+  auto in_shape = in_type.mutable_tensor_type()->mutable_shape();
+  for (int64_t dim : { 1, 224, 224, 3 })
+    in_shape->add_dim()->set_dim_value(dim);
+
+  ONNX_NAMESPACE::TypeProto out_type;
+  out_type.mutable_tensor_type()->set_elem_type(ONNX_NAMESPACE::TensorProto_DataType_FLOAT);
+  auto out_shape = out_type.mutable_tensor_type()->mutable_shape();
+  for (int64_t dim : { 1, 230, 230, 3 })
+    out_shape->add_dim()->set_dim_value(dim);
+
+  NodeArg in_arg = { "in", &in_type };
+  NodeArg out_arg = { "out", &out_type };
+  auto& node = graph.AddNode("node1", "Pad", "Pad", { &in_arg} , { &out_arg }, nullptr, onnxruntime::kOnnxDomain);
+  node.AddAttribute("pads", std::vector<int64_t>{0, 3, 3, 0, 0, 3, 3, 0});
+  node.AddAttribute("value", 0.0f);
+
+  EXPECT_TRUE(graph.Resolve().IsOK());
+  std::stringstream s1;
+  model->ToProto().SerializeToOstream(&s1);
+
+  // load the model
+  SessionOptions so;
+  so.session_logid = "Pad";
+  so.enable_profiling = true;
+  InferenceSession session_object{so};
+  EXPECT_TRUE(session_object.RegisterExecutionProvider(DefaultCpuExecutionProvider()).IsOK());
+  EXPECT_TRUE(session_object.Load(s1).IsOK());
+  EXPECT_TRUE(session_object.Initialize().IsOK());
+
+  auto allocator = test::AllocatorManager::Instance().GetAllocator(CPU);
+
+  // prepare inputs
+  MLValue ml_value;
+  CreateMLValue<float>(allocator, {1,224,224,3}, std::vector<float>(1*224*224*3, 0.0f), &ml_value);
+  NameMLValMap feeds;
+  feeds.insert(std::make_pair("in", ml_value));
+
+  // prepare outputs
+  std::vector<std::string> output_names;
+  output_names.push_back("out");
+  std::vector<MLValue> fetches;
+
+  RunOptions run_options;
+
+  // measure Pad performance
+  uint64_t elapsed = 0;
+  for (size_t i = 0; i < 1000; i++) {
+    auto start = std::chrono::high_resolution_clock::now();
+    common::Status status = session_object.Run(run_options, feeds, output_names, &fetches);
+    auto end = std::chrono::high_resolution_clock::now();
+    elapsed += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+    EXPECT_TRUE(status.IsOK());
+    auto& rtensor = fetches.front().Get<Tensor>();
+    TensorShape expected_shape({1, 230, 230, 3});
+    ASSERT_EQ(expected_shape, rtensor.Shape());
+  }
+
+  std::cout << "Pad elapsed: " << (elapsed / 1000) << "us" << std::endl;
+  std::cout << session_object.EndProfiling() << std::endl;
+}
 
 TEST(TensorOpTest, Pad_Spec_Example) {
   OpTester test("Pad");
